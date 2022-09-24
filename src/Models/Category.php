d1062101d35861cef6e0833c781db062327fcf25
ousid <ousid@users.noreply.github.com>
2022-09-24 15:05:44 +0000

    Fix styling

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -5,8 +5,6 @@ namespace Coderflex\LaravelTicket\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Category extends Model
 {
